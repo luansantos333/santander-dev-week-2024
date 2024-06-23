48cf982189e8c7a461f0be304f7e86b80fda3827
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-06-23 19:24:17 -0300

    Adição exception handler

--- src/main/java/me/dio/domain/repository/UserRepository.java
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -8,6 +8,5 @@ import org.springframework.stereotype.Repository;
 
 public interface  UserRepository extends JpaRepository <User, Long> {
 
-    boolean existByAccountNumber(String accountNumber);
 
 }
